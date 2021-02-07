4c43cde6dae79c36993f54e4a4e2a3cc9c801e33
CostanTito <34768273+CostanTito@users.noreply.github.com>
2021-02-07 22:59:42 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,2 @@
-@echo off
 echo Please wait, attempting to install...
-cmd /c npm install && echo Installation Successful || echo Please ensure you have installed NodeJS. Download from https://nodejs.org/en/
-pause
-
+npm install && echo Installation Successful || echo Please ensure you have installed NodeJS. Download from https://nodejs.org/en/
